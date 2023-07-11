ba95f0abd2b5e3c84eb733420210e294918f3238
alex <format37@gmail.com>
2023-07-11 12:10:18 +0400

    modified:   client.py

--- client.py
+++ client.py
@@ -1,23 +1,36 @@
 import requests
 import datetime
+import sys
+import os
 
-# Replace with your Flask server's URL
-url = 'http://127.0.0.1:6000'
+def main():
+    # Check if the IP address was passed as a command-line argument
+    if len(sys.argv) < 2:
+        print("Usage: python client.py <server-ip>")
+        sys.exit(1)
 
-# Define some test data
-test_data = {
-    '$i': 'incomers',
-    '$o': 'outgoers',
-    '$n': 'attendees',
-    '$s': 'counter serial no.',
-    '$c': 'card ID',
-    '$k': 'card shift start and finish',
-    '$d': 'UTC time in sec',
-    '$m': 'MD5 hash of summary line with signature added'
-}
+    # Get the server's IP address from the command-line argument
+    server_ip = sys.argv[1]
+    url = f'http://{server_ip}:6000'
 
-# Send a POST request to the server
-response = requests.post(url, json=test_data)
+    # Define some test data
+    test_data = {
+        '$i': 'incomers',
+        '$o': 'outgoers',
+        '$n': 'attendees',
+        '$s': 'counter serial no.',
+        '$c': 'card ID',
+        '$k': 'card shift start and finish',
+        '$d': 'UTC time in sec',
+        '$m': 'MD5 hash of summary line with signature added'
+    }
 
-# Print the server's response
-print(response.text)
+    # Send a POST request to the server
+    response = requests.post(url, json=test_data)
+
+    # Print the server's response
+    print(response.text)
+
+
+if __name__ == '__main__':
+    main()
